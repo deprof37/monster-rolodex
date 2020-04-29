d851c8d2360b9bbcd821ab34fd2768974db65014
mekunmekun <olusolafatoy@gmail.com>
2020-04-29 02:02:32 +0100

    Updates

---
+++ precache-manifest.58e87652b3c66ced0e913f0eccb8bb69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4ae87ffffefc13f56dae4a7d49f4ca",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b7cedba0eee2100922a1",
+    "url": "/monster-rolodex/static/css/main.556fee92.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7cedba0eee2100922a1",
+    "url": "/monster-rolodex/static/js/main.b7600854.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
